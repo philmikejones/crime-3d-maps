f6fb136e802ad2f21429ad82c086d037ef02d72f
Phil Mike Jones <philmikejones@gmail.com>
2021-09-08 21:15:31 +0100

    rename crime data script

--- src/data/01-prep-crime-data.r
+++ src/data/01-prep-crime-data.r
@@ -1,3 +1,6 @@
+library("dplyr")
+library("sf")
+
 # Download crime data from data.police.uk
 # Here I've downloaded all crime for the 12 months January - December 2020
 # for all forces
@@ -8,18 +11,5 @@ if (length(list.files("data/external/crimes", recursive = TRUE)) < 1L)
     unzip(
         "data/external/926a6fb5fc9ef52299cbe0134e9faa7efbd3d253.zip",
         exdir = "data/external/crimes"
-    )
-}
-
-if (!file.exists("data/external/force_kmls.zip"))
-{
-    download.file(
-        "https://data.police.uk/data/boundaries/force_kmls.zip",
-        destfile = "data/external/force_kmls.zip"
-    )
-
-    unzip(
-        "data/external/force_kmls.zip",
-        exdir = "data/external"
     )
 }
