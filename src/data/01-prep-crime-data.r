6cd94aaf4a3d7bb0dfce601ce2d188eb49919356
Phil Mike Jones <philmikejones@gmail.com>
2021-09-08 21:39:10 +0100

    save crimes.rds to interim

--- src/data/01-prep-crime-data.r
+++ src/data/01-prep-crime-data.r
@@ -1,5 +1,5 @@
 library("dplyr")
-library("sf")
+library("readr")
 
 # Download crime data from data.police.uk
 # Here I've downloaded all crime for the 12 months January - December 2020
@@ -13,3 +13,14 @@ if (length(list.files("data/external/crimes", recursive = TRUE)) < 1L)
         exdir = "data/external/crimes"
     )
 }
+
+crimes = list.files("data/external/crimes", recursive = TRUE, full.names = TRUE)
+crimes = lapply(crimes, read_csv)
+crimes = bind_rows(crimes)
+crimes = 
+    crimes %>%
+    filter(`Crime type` == "Burglary") %>%
+    select(`Crime ID`, Longitude, Latitude, `LSOA code`, `Crime type`)
+
+dir.create("data/interim", showWarnings = FALSE, recursive = TRUE)
+saveRDS(crimes, "data/interim/crimes.rds")
