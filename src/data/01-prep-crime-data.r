88306179f97f6b43549f47f810a6e89a2377f5f4
Phil Mike Jones <p.jones2@derby.ac.uk>
2021-09-09 15:53:40 +0100

    use 2018-06 to 2019-06 data (no data for Manchester beyond 2019-07)

--- src/data/01-prep-crime-data.r
+++ src/data/01-prep-crime-data.r
@@ -1,22 +1,29 @@
+library("assertthat")
 library("dplyr")
 library("readr")
 
 # Download crime data from data.police.uk
-# Here I've downloaded all crime for the 12 months January - December 2020
-# for all forces
+# Here I've downloaded all crime for the 11 months August 2018 - June 2019
+# Why not more recent? Greater Manchester don't report any crimes from July 2019
+# onwards
+# Why not 12 months (i.e. from July 2018)? Data doesn't go back that far
+# https://data.police.uk/changelog/
 dir.create("data/external/crimes", showWarnings = FALSE, recursive = TRUE)
 
 if (length(list.files("data/external/crimes", recursive = TRUE)) < 1L)
 {
     unzip(
-        "data/external/926a6fb5fc9ef52299cbe0134e9faa7efbd3d253.zip",
+        "data/external/0256321538e3cbb59afe05d8eed2d6cea5274e66.zip",
         exdir = "data/external/crimes"
     )
 }
 
 crimes = list.files("data/external/crimes", recursive = TRUE, full.names = TRUE)
-crimes = lapply(crimes, read_csv)
+crimes = lapply(crimes, read_csv, show_col_types = FALSE)
 crimes = bind_rows(crimes)
+
+assert_that(sum(str_detect(crimes$`Falls within`, "Manchester")) >= 1L)
+
 crimes = 
     crimes %>%
     filter(`Crime type` == "Burglary") %>%
